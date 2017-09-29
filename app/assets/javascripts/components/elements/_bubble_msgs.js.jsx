2dbabb6db19bd8e71ba5d3f2344555e726650ce6
Mathieu Chan <mathieu77197@gmail.com>
2017-09-28 20:29:45 -0400

    Changed the notices into React format
    - Added the React component BubbleMessages for alerts
    - Changed notices into their React counterpart

---
+++ app/assets/javascripts/components/elements/_bubble_msgs.js.jsx
@@ -0,0 +1,20 @@
+class BubbleMessage extends React.Component {
+    render(type, text) {
+        if(typeof text !== "string") return null;
+        return(
+            <div className={"alert alert-" + type}>{text}</div>
+        );
+    }
+}
+
+class Alert extends BubbleMessage {
+    render() {
+        return super.render("danger", this.props.text);
+    }
+}
+
+class Notice extends BubbleMessage {
+    render() {
+        return super.render("warning", this.props.text);
+    }
+}
