590504e641dd7f1c8ee2bcd474c48cd13f36809a
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2017-10-06 10:40:23 -0400

    Add email to production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'log210-equipe07.herokuapp.com', port: 80 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      user_name:     'Log210gr07@gmail.com',
+      password:      'PASSGR07',
+      domain:        'log210-equipe07.herokuapp.com',
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
+
 end
