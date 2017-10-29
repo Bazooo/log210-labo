f55bd68a37d55c50a23cdc431c69811b90d71fa3
Unknown <michel.pelland12@gmail.com>
2017-10-29 09:08:31 -0700

    Small fixes and validations
    -removed front-end validation for role
    -added fields validation for organisms

--- app/models/organism.rb
+++ app/models/organism.rb
@@ -1,4 +1,9 @@
 class Organism < ApplicationRecord
     has_one :user
 
+
+    validates :name, :presence =>true, :allow_blank => false
+    validates :address, :presence =>true, :allow_blank => false
+    validates :phone, :presence =>true, :allow_blank => false
+    validates :email, :presence =>true, :allow_blank => false
 end
