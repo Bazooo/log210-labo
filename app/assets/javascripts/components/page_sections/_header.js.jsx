e627dbfb716ce8bcea6da092c04859b34d445471
Mathieu Chan <mathieu77197@gmail.com>
2017-12-07 23:41:30 -0500

    Add header link

--- app/assets/javascripts/components/page_sections/_header.js.jsx
+++ app/assets/javascripts/components/page_sections/_header.js.jsx
@@ -1,14 +1,42 @@
 class Header extends React.Component {
     render() {
-        let logout;
+        let logout, nav;
         if(this.props.signed) {
             logout = <RestLink path={this.props.destroyPath} method="delete" content="Logout" className="btn btn-primary"/>;
+            let organismLink, reforganismsLink;
+            if(this.props.role == "directeur") {
+                organismLink = <Link root={this.props.root} link="user_profiles/edit" title="Edit profile" />;
+            }
+            if(this.props.role == "directeur" || this.props.role == "coordonateur") {
+                reforganismsLink = <Link root={this.props.root} link="reforganisms" title="Référents organismes" />;
+            }
+            nav = (
+                <ul className="nav nav-pills">
+                    <Link root={this.props.root} link="" title="Main" />
+                    <Link root={this.props.root} link="user_profiles/edit" title="Edit profile" />
+                    {organismLink}
+                    {reforganismsLink}
+                    <Link root={this.props.root} link="referent_searches/new" title="Chercher référents" />
+                </ul>
+            );
         }
         return(
             <header className="page-header">
-                <h1>RQRSDA <small>{this.props.title}</small></h1>
+                <h1>RQRSDA</h1>
+                {nav}
                 {logout}
             </header>
+        );
+    }
+}
+
+class Link extends React.Component {
+    render() {
+        const link = this.props.root + this.props.link;
+        return(
+            <li>
+                <a href={link}>{this.props.title}</a>
+            </li>
         );
     }
 }
--- test/controllers/user_profiles_controller_test.rb
+++ test/controllers/user_profiles_controller_test.rb
@@ -1,9 +1,43 @@
 require 'test_helper'
 
 class UserProfilesControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
-    #get user_profiles_edit_url
-    #assert_response :success
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user_phone = user_phones(:one);
+    @diploma = diplomas(:one);
+    @user_profile = user_profiles(:one);
   end
 
+  test "should redirect put update if not connected" do
+    put user_profiles_update_url
+    assert_redirected_to new_user_session_path
+  end
+
+  # test "should put update if connected" do
+  #   sign_in users(:one)
+  #
+  #   put user_profiles_update_path, params: {
+  #     :first_name => "Jaja",
+  #     :last_name => "Lala",
+  #     :address => "1234 Candyland",
+  #     :organism_role => "Dondon",
+  #     :user_phone_attributes => {
+  #       :cell_number => "123456",
+  #       :home_number => "123456",
+  #       :work_number => "123456"
+  #     },
+  #     :diploma_attributes => {
+  #       :program_name => "abc",
+  #       :institution_name => "asd",
+  #       :address => "123 asd",
+  #       :date_start => "2007-01-01",
+  #       :date_end => "2011-01-01"
+  #     }
+  #   };
+  #   assert_response :success
+  # end
+  # :first_name, :last_name, :address, :organism_role,
+  #   user_phone_attributes: [:cell_number, :home_number, :work_number],
+  #   diploma_attributes: [:program_name, :institution_name, :address, :date_start, :date_end]
 end
